1e1836eb4641e71af72be225c60f39b5a9866067
alsotang <alsotang@gmail.com>
2013-03-24 13:17:44 +0800

    add cron.yaml and queue.yaml

--- api_handler.py
+++ api_handler.py
@@ -1,14 +1,22 @@
+# coding=utf-8
+
 import webapp2
 
 import json
 import logging
 
 from model import PageContent
+from fetch_config import config as fetch_config
 
 class ApiHandler(webapp2.RequestHandler):
     def get(self, site_id=None, count=None):
         if (not site_id) or (not count):
-            self.response.write('How to use this API')
+            api_page = """<p>scunews API 的使用方法为：</p>
+            <p>GET /api/:site_id/:count ，site_id 是站点的前缀，count 是获取的数量。返回的数据格式为 JSON。目前支持的 sites 如下：</p>        
+            """ + ''.join(["<p><a href=\"/api/%s/5\">\
+            GET /%s/5</a></p>" % (site_id, site_id) for site_id, site_attrs in fetch_config.iteritems()])
+
+            self.response.write(api_page)
             return
 
         count = int(count)
